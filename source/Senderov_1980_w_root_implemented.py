02a2dce1893f119ea137cdaf02098ae9f3f3bdb1
Bob Myhill <myhill.bob@gmail.com>
2020-03-30 22:55:39 +0100

    ceated solution model object

--- source/Senderov_1980_w_root_implemented.py
+++ source/Senderov_1980_w_root_implemented.py
@@ -46,12 +46,13 @@ u = u.flatten()
 
 occ = Matrix(element_occupancies)
 element_occupancies_ind = np.array(occ.T.columnspace(), dtype='int')
+"""
 element_occupancies_ind = np.array([[1, 0, 0, 1, 0, 1, 0, 1],
                                     [0, 1, 1, 0, 0, 1, 0, 1],
                                     [0, 1, 0, 1, 1, 0, 0, 1],
                                     [0, 1, 0, 1, 0, 1, 1, 0],
                                     [0, 1, 0, 1, 0, 1, 0, 1]])
-
+"""
 E_ind = np.linalg.lstsq(element_occupancies_ind.T,
                         element_occupancies.T,
                         rcond=None)[0].round(decimals=10).T
@@ -61,14 +62,61 @@ def cluster_proportions(mu, T):
     return np.exp(np.where(lnval>100, 100., lnval))
 
 def delta_proportions_minimize(mu, p_ind, T):
-    Mbar = cluster_proportions(mu, T)
-    deltas = p_ind - E_ind.T.dot(Mbar)
+    p_cl = cluster_proportions(mu, T)
+    deltas = p_ind - E_ind.T.dot(p_cl)
     return np.sum(deltas**2)
 
 def delta_proportions_root(mu, p_ind, T):
-    Mbar = cluster_proportions(mu, T)
-    deltas = p_ind - E_ind.T.dot(Mbar)
+    p_cl = cluster_proportions(mu, T)
+    deltas = p_ind - E_ind.T.dot(p_cl)
     return deltas
+
+def inv_J(mu, p_ind, T):
+    p_cl = cluster_proportions(mu, T)
+    return np.einsum('ik, ij, i -> kj', E_ind, E_ind, p_cl)
+
+def dpcl_dp_ind(mu, p_ind, T):
+    p_cl = cluster_proportions(mu, T)
+    dp_ind_dmu = np.einsum('ik, ij, i -> kj', E_ind, E_ind, p_cl)
+    dpcl_dmu = np.einsum('ij, i -> ij', E_ind, p_cl)
+
+    # the returned solve is equivalent to
+    # np.einsum('lj, jk -> lk', dpcl_dmu, np.linalg.pinv(dp_ind_dmu)))
+    return np.linalg.solve(dp_ind_dmu.T, dpcl_dmu.T).T
+
+def chemical_potentials(mu, p_ind, T):
+    p_cl = cluster_proportions(mu, T)
+    ps = element_occupancies_ind.T.dot(p_ind)
+    D = dpcl_dp_ind(mu, p_ind, T)
+    g_E = np.einsum('lk, l', D, u)
+    g_S_n = -R*np.einsum('lk, l', D, logish(p_cl) + np.sum(p_cl))
+    g_S_i = -R*(np.einsum('ik, i', element_occupancies_ind.T, logish(ps)) + np.sum(ps))
+    g_S_t = gamma*g_S_n + (1./n - gamma)*g_S_i
+    print(g_S_t)
+    exit()
+    mu = g_E - T*g_S_t
+    print(element_occupancies_ind.T)
+    print(n, gamma, g_S_i)
+    #return np.array([act_G, act_E, act_S_t, act_S_i, act_S_n])
+    return mu
+
+
+def gibbs_entropies(mu, p_ind, T):
+    p_cl = cluster_proportions(mu, T)
+    ps = element_occupancies_ind.T.dot(p_ind)
+
+    S_n = -R*np.sum(p_cl*logish(p_cl))
+    S_i = -R*np.sum(ps*logish(ps))
+    #print(S_n, S_i)
+    #print(guess(p_ind, T))
+    n = 1. # 1 cluster total
+    E_t = np.sum(p_cl*u)
+    S_t = gamma*S_n + (1./n - gamma)*S_i
+
+    G_t = E_t - T*S_t
+
+    #return np.array([G_t, E_t, S_t, S_i, S_n])
+    return G_t
 
 def logish(x, eps=1.e-5):
     """
@@ -105,7 +153,7 @@ def guess(p_ind, T):
 
 
 
-ys = np.linspace(0., 1., 101)
+ys = np.linspace(0.4, 1., 101)
 
 # S_n = -2.*0.5*np.log(0.5)
 # S_i = -8.*0.5*np.log(0.5)
@@ -116,7 +164,7 @@ ps_to_p_ind = np.linalg.pinv(element_occupancies_ind.T)
 fig = plt.figure()
 ax = [fig.add_subplot(2, 2, i) for i in range(1, 5)]
 
-for T in np.linspace(100., 2000., 39):
+for T in np.linspace(1600., 2000., 39):
     print('\r{0} K'.format(T), end='')
     sols = []
     for y in ys:
@@ -126,6 +174,41 @@ for T in np.linspace(100., 2000., 39):
                        y, 1.-y])
         p_ind = ps_to_p_ind.dot(ps)
         sol = root(delta_proportions_root, guess(p_ind, T), args=(p_ind, T))
+
+        for l in np.identity(5)*1.e-8:
+            sol2 = root(delta_proportions_root,
+                        guess(p_ind + l, T),
+                        args=(p_ind + l, T))
+            print((sol2.x - sol.x)*1.e8)
+        print(np.linalg.pinv(inv_J(sol.x, p_ind, T)))
+
+        pcl = cluster_proportions(sol.x, T)
+
+        for l in np.identity(5)*1.e-8:
+            sol2 = root(delta_proportions_root,
+                        guess(p_ind + l, T),
+                        args=(p_ind + l, T))
+            pcl2 = cluster_proportions(sol2.x, T)
+            print((pcl2 - pcl)*1.e8)
+
+        print(dpcl_dp_ind(sol.x, p_ind, T).T)
+
+        for l in np.identity(5)*1.e-8:
+            sol2 = root(delta_proportions_root,
+                        guess(p_ind + l, T),
+                        args=(p_ind + l, T))
+            print((gibbs_entropies(sol2.x, p_ind + l, T)
+                   - gibbs_entropies(sol.x, p_ind, T))*1.e8)
+
+
+        print(chemical_potentials(sol.x, p_ind, T))
+
+        print(np.einsum('i, i', p_ind, chemical_potentials(sol.x, p_ind, T)))
+        print(gibbs_entropies(sol.x, p_ind, T))
+
+
+
+        exit()
         if sol.success or np.max(np.abs(sol.fun)) < 1.e-10: #or sol.fun < 1.e-6:
             mu = sol.x
             #mu = guess(p_ind, T)
